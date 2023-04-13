98d551ffb161cc42713396434ae35613b0c953b9
jeongola <jeongola@naver.com>
2023-04-13 13:51:58 +0900

    1.7

--- 1.5/main.go
+++ 1.5/main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func superAdd(numbers ...int) int {
-	for number := range numbers {
-		fmt.Println(number)
+	total := 0
+	for _, number := range numbers {
+		total += number
 	}
-	return 1
+	return total
 }
 
 func main() {
-	superAdd(1, 2, 3, 4, 5, 6)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
---
+++ 1.7/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func canIDrink(age int) bool {
+	switch age {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(canIDrink(18))
+}
