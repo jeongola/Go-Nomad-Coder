a465c6b5a53cc1ed2f7f41b24abb7cd16f8a07e5
jeongola <jeongola@naver.com>
2023-04-13 13:55:09 +0900

    1.7

--- 1.7/main.go
+++ 1.7/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	switch age {
-	case 10:
+	switch {
+	case age < 18:
 		return false
-	case 18:
+	case age == 18:
 		return true
+	case age > 50:
+		return false
 	}
 	return false
 }
