148a67963d4febe22d1f9aea36141200a9d29ba8
jeongola <jeongola@naver.com>
2023-04-14 14:37:03 +0900

    3.0

---
+++ Project3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	urls := []string{"https://www.airbnb.co.kr/",
+		"https://www.coupang.com/",
+		"https://www.youtube.com/",
+		"https://www.naver.com/",
+		"https://www.instagram.com/",
+		"https://www.facebook.com/",
+	}
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err == nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
+}
