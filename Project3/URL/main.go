f3f0bfee63f75915fdf4cb167f5496145db49621
jeongola <jeongola@naver.com>
2023-04-14 15:58:40 +0900

    project3

--- Project3/URL/main.go
+++ Project3/URL/main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{"https://www.airbnb.co.kr/",
 		"https://www.coupang.com/",
 		"https://www.youtube.com/",
@@ -16,16 +17,27 @@ func main() {
 		"https://www.instagram.com/",
 		"https://www.facebook.com/",
 	}
+
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
+
 	return nil
 }
