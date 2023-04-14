c13215de42dd83a84fb6df8b759d802c1a5eb6df
jeongola <jeongola@naver.com>
2023-04-14 16:14:54 +0900

    3.4

--- Project3/Goroutines/main.go
+++ Project3/Goroutines/main.go
@@ -6,18 +6,22 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
+	c := make(chan string)
 	people := [2]string{"nico", "flynn"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+
+	fmt.Println("Waiting for messages")
+	resultOne := <-c
+	resultTwo := <-c
+	fmt.Println("Received this message:", resultOne)
+	fmt.Println("Received this message:", resultTwo)
+
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
 	fmt.Println(person)
-	c <- true
+	c <- person + " is sexy"
 }
